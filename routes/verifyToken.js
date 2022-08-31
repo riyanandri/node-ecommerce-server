5b06a8b8d36c6f896abc2ba3f4f27e25d7755d9f
riyanandri <hiddenfoxe01@gmail.com>
2022-08-31 19:47:33 +0700

    push all

---
+++ routes/verifyToken.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token tidak valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Anda tidak diautentikasi!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Anda tidak diizinkan untuk login!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Anda tidak diizinkan untuk login!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
